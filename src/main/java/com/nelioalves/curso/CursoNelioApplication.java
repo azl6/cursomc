809a9151e5d45052450b4af6c89723e6e2872363
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2021-08-30 23:44:45 -0300

    Projeto criado

---
+++ src/main/java/com/nelioalves/curso/CursoNelioApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelioalves/curso/CursoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7f29f15de236f2f3189dc90a60984df230b58f26
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2021-09-04 15:16:07 -0300

    Associação muitos-para-muitos entre duas classes

--- src/main/java/com/nelioalves/curso/CursoNelioApplication.java
+++ src/main/java/com/nelioalves/curso/CursoNelioApplication.java
@@ -1,7 +1,9 @@
 package com.nelioalves.curso;
 
 import com.nelioalves.curso.domain.Categoria;
+import com.nelioalves.curso.domain.Produto;
 import com.nelioalves.curso.repositories.CategoriaRepository;
+import com.nelioalves.curso.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,9 +14,12 @@ import java.util.Arrays;
 @SpringBootApplication
 public class CursoNelioApplication implements CommandLineRunner {
 
+	@Autowired
+	ProdutoRepository produtoRepository;
+
 	//CategoriaRepository responsável pela manipulação dos dados
 	@Autowired
-	CategoriaRepository cr;
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoNelioApplication.class, args);
@@ -24,6 +29,20 @@ public class CursoNelioApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria("Informática");
 		Categoria cat2 = new Categoria("Escritório");
-		cr.saveAll(Arrays.asList(cat1,cat2));
+
+
+		Produto p1 = new Produto("Computador", 2000d);
+		Produto p2 = new Produto("Impressora", 800d);
+		Produto p3 = new Produto( "Mouse", 80d);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
---
+++ src/main/java/com/nelioalves/curso/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.nelioalves.curso.repositories;
+
+import com.nelioalves.curso.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}

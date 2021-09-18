64795f6d862d107a74dd9f864085831a63c4e633
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2021-09-18 20:58:16 -0300

    Inserindo uma nova Categoria com o POST

--- src/main/java/com/nelioalves/curso/services/CategoriaService.java
+++ src/main/java/com/nelioalves/curso/services/CategoriaService.java
@@ -5,8 +5,11 @@ import com.nelioalves.curso.repositories.CategoriaRepository;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 //aula 17
@@ -19,5 +22,9 @@ public class CategoriaService {
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(1, "Página não encontrada!"));
+    }
+
+    public Categoria insert(Categoria obj){
+        return repo.save(obj);
     }
 }

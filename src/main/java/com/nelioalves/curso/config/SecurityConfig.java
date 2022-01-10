52bcd1c342464cc354c7ce257b4877892d486c09
Alex Rod <alex.rodrigues23997@gmail.com>
2022-01-10 15:15:54 -0300

    Implementando autorização

--- src/main/java/com/nelioalves/curso/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/curso/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.nelioalves.curso.config;
 
 import com.nelioalves.curso.security.JWTAuthenticationFilter;
+import com.nelioalves.curso.security.JWTAuthorizationFilter;
 import com.nelioalves.curso.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 

a28d9192fb0264c59e2a09e3f21401614fbf589f
Alex Rod <alex.rodrigues23997@gmail.com>
2022-01-17 15:34:59 -0300

    Salvando o primeiro arquivo no S3

--- src/main/java/com/nelioalves/curso/CursoNelioApplication.java
+++ src/main/java/com/nelioalves/curso/CursoNelioApplication.java
@@ -4,6 +4,7 @@ import com.nelioalves.curso.domain.*;
 import com.nelioalves.curso.enums.EstadoPagamento;
 import com.nelioalves.curso.enums.TipoCliente;
 import com.nelioalves.curso.repositories.*;
+import com.nelioalves.curso.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,12 +16,16 @@ import java.util.Arrays;
 @SpringBootApplication
 public class CursoNelioApplication implements CommandLineRunner {
 
+
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursoNelioApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+	s3Service.uploadFile("C:\\temp\\hjkm.png");
 	}
 }

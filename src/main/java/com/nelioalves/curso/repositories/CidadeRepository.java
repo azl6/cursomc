21827fa2d9ebedcf08792ee1d7e352243ca12497
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-01-20 10:36:35 -0300

    Endpoint para buscar estados e cidades

--- src/main/java/com/nelioalves/curso/repositories/CidadeRepository.java
+++ src/main/java/com/nelioalves/curso/repositories/CidadeRepository.java
@@ -2,6 +2,15 @@ package com.nelioalves.curso.repositories;
 
 import com.nelioalves.curso.domain.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+    @Transactional(readOnly=true)
+    @Query("SELECT obj FROM Cidade obj WHERE obj.estado.id = :estadoId ORDER BY obj.nome")
+    public List<Cidade> findCidades(@Param("estadoId") Integer estado_id);
 }
---
+++ src/main/java/com/nelioalves/curso/services/CidadeService.java
@@ -0,0 +1,19 @@
+package com.nelioalves.curso.services;
+
+import com.nelioalves.curso.domain.Cidade;
+import com.nelioalves.curso.repositories.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CidadeService {
+
+    @Autowired
+    private CidadeRepository repo;
+
+    public List<Cidade> findByEstado(Integer estadoId) {
+        return repo.findCidades(estadoId);
+    }
+}
